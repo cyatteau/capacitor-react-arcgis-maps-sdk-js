cc47f6644537a6dbea532fc16fff46444c34652b
cyatteau <112517097+cyatteau@users.noreply.github.com>
2023-01-03 20:01:19 -0500

    Add in ArcGIS Maps SDK and Capacitor

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useRef, useEffect } from "react";
+import ArcGISMap from "@arcgis/core/Map";
+import esriConfig from "@arcgis/core/config";
+import MapView from "@arcgis/core/views/MapView";
 
 function App() {
+  const mapContainer = useRef(null);
+  esriConfig.apiKey = "YOUR_API_KEY";
+  useEffect(() => {
+    if (mapContainer.current) {
+      const map = new ArcGISMap({
+        basemap: "gray-vector",
+      });
+      const view = new MapView({
+        container: mapContainer.current,
+        map: map,
+        zoom: 10,
+        center: [-77.1, 38.85],
+      });
+    }
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>ArcGIS JS MAP using Capacitor</h1>
+      <div ref={mapContainer} className="map"></div>
     </div>
   );
 }
